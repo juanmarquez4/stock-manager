d9aa7212406bed5e75122acc7f82fb69bb17035b
Juan Marquez <juanmarquez4@gmail.com>
2022-10-15 11:38:03 +1100

    Update App.js with header/nav/footer sections

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,35 @@
+import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   createHttpLink,
+// } from '@apollo/client';
+// import { setContext } from '@apollo/client/link/context';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      {/* header */}
+      <div className='header'>
+        <h1>Stock Manager</h1>
+        <div className='login'>
+          <h1>Sign Up</h1> 
+          <h1>Login</h1>
+        </div>
+      </div>
+      {/* navigation */}
+      <div className='nav'>
+        <h1>Home</h1>
+        <h1>Categories</h1> 
+        <h1>Products</h1>
+        <h1>Purchases</h1>
+        <h1>Sales</h1>
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +44,10 @@ function App() {
           Learn React
         </a>
       </header>
+       {/* footer */}
+      <div className='footer'>
+        <h1>Developed by Juan Marquez</h1>
+      </div>
     </div>
   );
 }
