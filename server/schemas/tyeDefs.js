5e321953a294ec13ed869b067912845f71ab8495
Juan Marquez <juanmarquez4@gmail.com>
2022-10-16 12:15:19 +1100

    Created models/index.js & Profile.js

--- server/schemas/tyeDefs.js
+++ server/schemas/tyeDefs.js
@@ -1,13 +1,28 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+type Profile {
+    _id: ID
+    name: String
+    email: String
+    password: String
+    skills: [String]!
+}
+
+type Auth {
+    token: ID!
+    profile: Profile
+}
 
 type Query {
-
+    profiles: [Profile]!
+    profile(profileId: ID!): Profile
 }
 
 type Mutation {
-
+    addProfile(name: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    removeProfile(profileId: ID!): Profile
 }
 `;
 
