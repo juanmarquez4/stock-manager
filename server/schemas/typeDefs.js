f9aa69a7ff8b050f25f5a40427f5d8decf129be8
Juan Marquez <juanmarquez4@gmail.com>
2022-10-18 08:08:27 +1100

    Updated server/schemas/typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,20 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+type Category {
+    _id: ID
+    name: String
+  }
+
+type Product {
+    _id: ID
+    name: String
+    description: String
+    image: String
+    quantity: Int
+    category: Category
+}
+
 type Profile {
     _id: ID
     name: String
@@ -14,6 +28,9 @@ type Auth {
 }
 
 type Query {
+    categories: [Category]
+    products(category: ID, name: String): [Product]
+    product(_id: ID!): Product
     profiles: [Profile]!
     profile(profileId: ID!): Profile
 }
@@ -22,7 +39,12 @@ type Mutation {
     addProfile(name: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
     removeProfile(profileId: ID!): Profile
+    addProduct(name: String!, description: String, image: String, quantity: Int): Product
+    updateProduct(_id: ID!, quantity: Int!): Product
+    addCategory(name: String!): Category
 }
 `;
 
 module.exports = typeDefs;
+
+// check if need to add Category to update product
