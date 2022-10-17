b84402a571107cf1986a511c2ceff3cfa0cbc595
Juan Marquez <juanmarquez4@gmail.com>
2022-10-18 07:42:42 +1100

    Update src/pages/Products.js

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -1,25 +1,39 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
-import logo from '../logo.svg';
 
 const Products = () => {
     return (
-        <main>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                  Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Learn React
-                </a>
-        </header>
-        </main>
+      <container className="Card">
+        <div className="Card-header" >
+          <h1>Products</h1>
+          <Link>
+            <h1>Create</h1>
+          </Link>
+        </div>
+        <div>
+          <form>
+            <input
+            placeholder="Enter product name"
+            />
+            <button>
+              Search
+            </button>
+          </form>
+        </div>
+        <div className="Card-body">
+          <table>
+            <div className="Card-header">
+                <h1>name</h1>
+                <h1>Description</h1>
+            </div>
+            <div className="Card-header">
+                <h1>Wine</h1>
+                <h1>comes from grapes</h1>
+            </div>
+          </table>
+        </div>
+      </container>
     );
 };
 
