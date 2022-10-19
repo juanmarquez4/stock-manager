7db26c85d661689c9c53178a7da95a34eda91e14
Juan Marquez <juanmarquez4@gmail.com>
2022-10-20 09:33:35 +1100

    Updated client/src/pages/Products.js

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -1,39 +1,49 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
 
 const Products = () => {
     return (
-      <container className="Card">
-        <div className="Card-header" >
-          <h1>Products</h1>
-          <Link>
-            <h1>Create</h1>
-          </Link>
-        </div>
-        <div>
-          <form>
-            <input
-            placeholder="Enter product name"
-            />
-            <button>
-              Search
-            </button>
-          </form>
-        </div>
-        <div className="Card-body">
-          <table>
-            <div className="Card-header">
-                <h1>name</h1>
-                <h1>Description</h1>
-            </div>
-            <div className="Card-header">
-                <h1>Wine</h1>
-                <h1>comes from grapes</h1>
-            </div>
-          </table>
-        </div>
-      </container>
+      <Container>
+        <Card>
+          <Card.Header>Use this form to add a Product</Card.Header>
+            <Container>
+              <Form >
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control type="text" placeholder="Enter product name" name="name" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Categories</Form.Label>
+                  <Form.Select aria-label="Default select example">
+                    <option>Select a Category for this product</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </Form.Select>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Items</Form.Label>
+                  <Form.Control placeholder="Enter number of items in store" name="items"  />
+                </Form.Group>
+               
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Description</Form.Label>
+                  <Form.Control type="text" placeholder="Enter product description" name="description"  />
+                </Form.Group>
+              
+                <Button variant="primary" type="submit">Click to add Product</Button>
+              </Form>
+            </Container>
+        </Card>
+      </Container>
     );
 };
 
