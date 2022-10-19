dc7367904b70c26a75a86ef291022e56df2169cd
Juan Marquez <juanmarquez4@gmail.com>
2022-10-20 08:09:34 +1100

    Updated client/src/pages/Categories.js

--- client/src/pages/Categories.js
+++ client/src/pages/Categories.js
@@ -1,39 +1,33 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
 
 const Categories = () => {
     return (
-      <container className="Card">
-        <div className="Card-header" >
-          <h1>Categories</h1>
-          <Link>
-            <h1>Create</h1>
-          </Link>
-        </div>
-        <div>
-          <form>
-            <input
-            placeholder="Enter category name"
-            />
-            <button>
-              Search
-            </button>
-          </form>
-        </div>
-        <div className="Card-body">
-          <table>
-            <div className="Card-header">
-                <h1>name</h1>
-                <h1>Description</h1>
-            </div>
-            <div className="Card-header">
-                <h1>Wine</h1>
-                <h1>comes from grapes</h1>
-            </div>
-          </table>
-        </div>
-      </container>
+      <Container>
+              <Card>
+              <Card.Header>Use this form to add a Category</Card.Header>
+                <Container>
+                  <Form >
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                      <Form.Label>Name</Form.Label>
+                      <Form.Control type="text" placeholder="Enter category name" name="name" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                      <Form.Label>Description</Form.Label>
+                      <Form.Control type="text" placeholder="Enter category description" name="description"  />
+                    </Form.Group>
+                  
+                    <Button variant="primary" type="submit">Click to add Category</Button>
+                  </Form>
+                </Container>
+              </Card>
+              </Container>
     );
 };
 
