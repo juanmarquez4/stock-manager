720dd01e26227ae406e9170693aaa9d71945b09f
Juan Marquez <juanmarquez4@gmail.com>
2022-10-19 21:27:08 +1100

    Changed Navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,58 +1,32 @@
 import React from "react";
-import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import {LinkContainer} from 'react-router-bootstrap'
 
 function Navv() {
     return (
         <>
-        <nav className='nav'>
-            <Link to="/">
-                <h1>Home</h1>
-            </Link>
-            <Link to="/categories">
-                <h1>Categories</h1>
-            </Link> 
-            <Link to="/products">
-                <h1>Products</h1>
-            </Link> 
-            <Link to="/purchases">
-                <h1>Purchases</h1>
-            </Link> 
-            <Link to="/sales">
-                <h1>Sales</h1>
-            </Link> 
-        </nav>
-          <Navbar bg="light" expand="lg">
+        <Navbar bg="light" expand="lg">
           <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand >Stock Manager</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
                 <LinkContainer to="/">
                     <Nav.Link href="#home">Home</Nav.Link>
                 </LinkContainer>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
-                </NavDropdown>
+                <LinkContainer to="/categories">
+                    <Nav.Link href="#link">Categories</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/products">
+                    <Nav.Link href="#link">Products</Nav.Link>
+                </LinkContainer>
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
         </>
-        
     );
 }
 
